8dd50217ce841fc2b262a43e86dd03d1285327ba
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-01-13 16:01:49 +0200

    Completed proposals data api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |proposal|
+	Proposal.create!(
+		customer: "Customer #{proposal}",
+		portfolio_url: string = 'https://github.com/ecuSinner',
+		tools: 'Ruby on Rails, Angular 2, and Postgres',
+		estimated_hours: rand(50..100),
+		hourly_rate: rand(35..50),
+		weeks_to_complete: rand(4..8),
+		client_email: 'pamirshk@gmail.com'
+	)
+end
+
+puts "10 proposals created"
